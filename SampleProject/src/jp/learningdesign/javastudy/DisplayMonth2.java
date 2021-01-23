0e2e667c8cce700eb78cdc957739cde51f64cc29
mackxxx <42348939+mackxxx@users.noreply.github.com>
2021-01-24 02:09:36 +0900

    継承

---
+++ SampleProject/src/jp/learningdesign/javastudy/DisplayMonth2.java
@@ -0,0 +1,13 @@
+package jp.learningdesign.javastudy;
+
+public class DisplayMonth2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String[] month = {"january","Februray","March","April"};
+		
+		for (int i = 0; i < 4 ;i++) {
+			System.out.println(month[i]);
+		}
+	}
+}
--- SampleProject/src/jp/learningdesign/javastudy/StaffInfo.java
+++ SampleProject/src/jp/learningdesign/javastudy/StaffInfo.java
@@ -8,17 +8,65 @@ class Staff{
 	public void  sayhello() {
 		System.out.println("Hello"  + this.name);
 	}
+
+	public Staff(String name, int staffid, String email) {
+		super();
+		this.name = name;
+		this.staffid = staffid;
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getStaffid() {
+		return staffid;
+	}
+
+	public void setStaffid(int staffid) {
+		this.staffid = staffid;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	
 }
-
+class RemoteStaff extends Staff{
+    public String location;
+	public RemoteStaff(String name, int staffid, String email) {
+		super(name, staffid, email);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+	
+}
 public class StaffInfo {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-      Staff yamada = new Staff();
-      yamada.name = "Taro yamada";
-     
-      yamada.sayhello();
+      Staff yamada = new Staff("Taro yamada", 12345, "yamada@gmail.com");
+      RemoteStaff tanaka = new RemoteStaff("Hanako Tabata", 123456, "tanbata@gmail.com");
+      tanaka.location = "大阪";
+      //yamada.sayhello();
+      System.out.println("【社員情報】");
+      System.out.println("氏名: " + yamada.getName());
+      System.out.println("社員番号 : " + yamada.getStaffid());
+      System.out.println("Email: "  + yamada.getEmail());
+	
+      System.out.println("【社員情報】");
+      System.out.println("氏名: " + tanaka.getName());
+      System.out.println("社員番号 : " + tanaka.getStaffid());
+      System.out.println("Email: "  + tanaka.getEmail());
+      System.out.println("勤務地 "  + tanaka.location);
 	}
 
 }
